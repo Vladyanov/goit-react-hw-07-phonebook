f358f6bf4631314d7292bb604179a530746ab422
Vlad Pazyna <vladpazyna@gmail.com>
2023-03-04 19:44:19 +0200

    first part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import PhoneBook from 'components/PhoneBook';
 
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={'...Loading'} persistor={persistor}>
-        <PhoneBook />
-      </PersistGate>
+      <PhoneBook />
     </Provider>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
